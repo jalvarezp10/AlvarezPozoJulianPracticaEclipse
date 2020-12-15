7bc14d24430e2cfcf38341845b08dd08e6662b16
jalvarezp10 <55429416+jalvarezp10@users.noreply.github.com>
2020-12-15 19:55:56 +0100

    Update prueba.java

--- Prueba/src/prueba.java
+++ Prueba/src/prueba.java
@@ -5,6 +5,7 @@ public class prueba {
     public static void main(String args[])
     {
         System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo desde GitHub");
     }
 }
 
